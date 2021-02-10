279138310a95a27b3902ee892e4aac0fd689a10c
Adroaldo <hygino@msn.com>
2021-02-10 17:06:31 -0300

    Iniciando a implementação

---
+++ ExerFixTratamento/Program.cs
@@ -0,0 +1,28 @@
+﻿using ExerFixTratamento.Entities;
+using System;
+using System.Globalization;
+
+namespace ExerFixTratamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter account data");
+            Console.WriteLine("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Holder: ");
+            string holder = Console.ReadLine();
+            Console.Write("Initial balance: ");
+            double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Withdraw limit: ");
+            double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Account acc = new Account(number, holder, balance, withdrawLimit);
+
+            Console.Write("Enter amount for withdraw: ");
+            double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //Console.Write("Withdraw error:");
+
+        }
+    }
+}
